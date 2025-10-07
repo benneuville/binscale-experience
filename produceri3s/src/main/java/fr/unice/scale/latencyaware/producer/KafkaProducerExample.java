d943319d0ab537fbf47dcb274db457c65b43701c
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2025-10-07 19:03:52 +0200

    log externalized + params & delete magic params

--- produceri3s/src/main/java/fr/unice/scale/latencyaware/producer/KafkaProducerExample.java
+++ produceri3s/src/main/java/fr/unice/scale/latencyaware/producer/KafkaProducerExample.java
@@ -7,36 +7,24 @@ import java.util.Random;
 import fr.unice.scale.latencyaware.common.entity.Customer;
 import fr.unice.scale.latencyaware.producer.config.KafkaProducerConfig;
 import fr.unice.scale.latencyaware.producer.constant.WorkloadMapping;
-import fr.unice.scale.latencyaware.producer.workload.BiasedWorkload;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import static fr.unice.scale.latencyaware.producer.constant.Variables.INPUT_WORKLOAD;
+import static fr.unice.scale.latencyaware.producer.constant.Variables.WORKLOAD;
 
 
 public class KafkaProducerExample {
     private static final Logger log = LogManager.getLogger(KafkaProducerExample.class);
 
-    public static KafkaProducerConfig config;
-    public static KafkaProducer<String, Customer> producer;
-    public static WorkloadMapping workloadMapping;
-
-    //random number gen
-    static Random rnd;
-
-
     public static void main(String[] args) throws InterruptedException, IOException, URISyntaxException {
-        rnd = new Random();
-        config = KafkaProducerConfig.fromEnv();
+        KafkaProducerConfig config = KafkaProducerConfig.fromEnv();
         log.info(KafkaProducerConfig.class.getName() + ": {}", config.toString());
         Properties props = KafkaProducerConfig.createProperties(config);
-        producer = new KafkaProducer<>(props);
+        KafkaProducer<String, Customer> producer = new KafkaProducer<>(props);
         startServer();
 
-        workloadMapping = WorkloadMapping.getByName(INPUT_WORKLOAD);
-
-        workloadMapping.getWorkload().startWorkload();
+        WORKLOAD.getWorkload().startWorkload(config, producer);
     }
 
     private static void startServer() {
