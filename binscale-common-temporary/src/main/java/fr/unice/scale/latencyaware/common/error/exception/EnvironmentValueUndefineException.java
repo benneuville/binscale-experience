71c7a4322a1acfc875bd357351bef3c9117b6398
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2025-10-10 11:58:07 +0200

    fix pom groupId for local execution

--- binscale-common-temporary/src/main/java/fr/unice/scale/latencyaware/common/error/exception/EnvironmentValueUndefineException.java
+++
@@ -1,8 +0,0 @@
-package fr.unice.scale.latencyaware.common.error.exception;
-
-public class EnvironmentValueUndefineException extends AbstractException {
-    public EnvironmentValueUndefineException(String key) {
-        super("Environment Variable is missing/undefined : " + key + "\n");
-    }
-
-}
--- binscale-common-temporary/src/main/java/fr/unice/scale/latencyaware/common/error/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package fr.unice.scale.latencyaware.common.error.exception;
-
-public class NotFoundException extends AbstractException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
